aa85a2e375a486a231a755327821399948d8efe2
unknown <kustova-liza02@mail.ru>
2022-01-03 19:58:34 +1000

    First mass filled

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿Console.WriteLine("Введите желаемое количество элементов в массиве и нажмите ENTER:");
 int a = Console.Read();
 int[] Mass1 = new int[a];
-
+Console.WriteLine("Заполните введённое количество элементов массива через ENTER:");
+for (int i = 1; i < a; i++)
+{
+    Mass1[i]= Console.Read();
+}
