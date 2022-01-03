f8f4902677748fe2281c54245c1d4da76ad61306
unknown <kustova-liza02@mail.ru>
2022-01-03 20:16:38 +1000

    Second mass added and filled

--- Program.cs
+++ Program.cs
@@ -2,7 +2,15 @@
 int a = Console.Read();
 int[] Mass1 = new int[a];
 Console.WriteLine("Заполните введённое количество элементов массива через ENTER:");
+int count = 0;
 for (int i = 1; i < a; i++)
 {
     Mass1[i]= Console.Read();
+    if (Mass1[i] % 2 == 0) {count++;};
+}
+int[] Mass2 = new int[count];
+int j = 0;
+for (int i = 1; i < a; i++)
+{
+    if (Mass1[i] % 2 == 0) {Mass2[j]=Mass1[i]; j++;}
 }
