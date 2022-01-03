4550291e0aa0cc0454f1e8b8dc180358d9a91e05
unknown <kustova-liza02@mail.ru>
2022-01-03 21:01:25 +1000

    error elimination

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
 ﻿Console.WriteLine("Введите желаемое количество элементов в массиве и нажмите ENTER:");
-int a = Console.Read();
+int a = Convert.ToInt32(Console.ReadLine());
 int[] Mass1 = new int[a];
 Console.WriteLine("Заполните введённое количество элементов массива через ENTER:");
 int count = 0;
-for (int i = 1; i < a; i++)
+for (int i = 0; i < a; i++)
 {
-    Mass1[i]= Console.Read();
+    Mass1[i]= Convert.ToInt32(Console.ReadLine());
     if (Mass1[i] % 2 == 0) {count++;};
 }
 int[] Mass2 = new int[count];
 int j = 0;
-for (int i = 1; i < a; i++)
+Console.WriteLine("Полученный массив из чётных элементов: ");
+for (int i = 0; i < a; i++)
 {
-    if (Mass1[i] % 2 == 0) {Mass2[j]=Mass1[i]; j++;}
+    if (Mass1[i] % 2 == 0) {Mass2[j]=Mass1[i]; Console.Write($"{Mass2[j]}    "); j++;};
 }
