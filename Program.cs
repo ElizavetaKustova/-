0b6222d53d09c41062e36129d1f421f53bb7edc6
unknown <kustova-liza02@mail.ru>
2022-01-03 22:17:55 +1000

    Improved to methods

--- Program.cs
+++ Program.cs
@@ -2,9 +2,22 @@
 {
     for (int n = 0; n < arr.Length; n++) {arr[n] = 0;};
 }
+
 Console.WriteLine("Введите желаемое количество элементов в массиве и нажмите ENTER:");
 int a = Convert.ToInt32(Console.ReadLine());
 int[] Mass1 = new int[a];
+
+void NewArr(int[] arr)
+{
+    Console.WriteLine("Полученный массив из чётных элементов: ");
+    int j = 0;
+    for (int i = 0; i < a; i++)
+    {
+        if (Mass1[i] % 2 == 0) {arr[j]=Mass1[i]; Console.Write($"{arr[j]}    "); j++;};
+    }
+    if (arr.Length == 0) {Console.WriteLine("В исходном массиве нет чётных элементов");};
+}
+
 Console.WriteLine("Заполните введённое количество элементов массива через ENTER:");
 int count = 0;
 for (int i = 0; i < a; i++)
@@ -13,17 +26,13 @@ for (int i = 0; i < a; i++)
     if (Mass1[i] % 2 == 0) {count++;};
 }
 int[] Mass2 = new int[count];
-int j = 0;
-Console.WriteLine("Полученный массив из чётных элементов: ");
-for (int i = 0; i < a; i++)
-{
-    if (Mass1[i] % 2 == 0) {Mass2[j]=Mass1[i]; Console.Write($"{Mass2[j]}    "); j++;};
-}
+NewArr(Mass2);
 Clear(Mass1);
 Clear(Mass2);
 count = 0;
 Console.WriteLine();
-Console.WriteLine("Эта программа также может заполнять исходный массив рандомными числами  типа int, например: ");
+
+Console.WriteLine("Эта программа также может заполнять исходный массив рандомными числами типа int, например: ");
 Random rand = new Random();
 for (int i = 0; i < a; i++)
 {
@@ -32,10 +41,5 @@ for (int i = 0; i < a; i++)
     if (Mass1[i] % 2 == 0) {count++;};
 }
 int[] Mass3 = new int[count];
-j = 0;
 Console.WriteLine();
-Console.WriteLine("Полученный массив из чётных элементов: ");
-for (int i = 0; i < a; i++)
-{
-    if (Mass1[i] % 2 == 0) {Mass3[j]=Mass1[i]; Console.Write($"{Mass3[j]}    "); j++;};
-}
+NewArr(Mass3);
